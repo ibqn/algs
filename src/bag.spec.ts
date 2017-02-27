984776958ec2570a7406fe19e6d9f0a2708022c9
Evgeny Bobkin <evgeny.bobkin@gmail.com>
2017-02-27 18:36:07 +0100

    improve bag test

--- src/bag.spec.ts
+++ src/bag.spec.ts
@@ -2,8 +2,9 @@ import { expect } from "chai";
 import { Bag } from "./bag";
 
  describe('bag tests', () => {
+  const bag = new Bag<number>();
+
   it('should be intialized', () => {
-    let bag = new Bag<number>();
     expect(bag).to.exist;
     expect(bag.isEmpty()).to.be.true;
     expect(bag.size()).to.be.equal(0);
