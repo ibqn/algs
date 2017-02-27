b9b0c9d2a4bf4c79f3f17a50a9125728678eadbf
Evgeny Bobkin <evgeny.bobkin@gmail.com>
2017-02-27 18:26:24 +0100

    add std-data and its tests

---
+++ src/std-data.spec.ts
@@ -0,0 +1,30 @@
+import { expect } from "chai";
+import { StdData } from "./std-data";
+
+ describe('std data tests', () => {
+  const content = "1 2 3\n4 5\r\n6 7\n 8 9\n10"
+
+  const stdData = new StdData(content);
+
+  beforeEach(() => {
+    stdData.reset();
+  });
+
+  it('should be intialized', () => {
+    expect(stdData).to.exist;
+    expect(stdData.empty()).to.be.false;
+    expect(stdData.size()).to.be.equal(10);
+  });
+
+  it('test get data', () => {
+
+    for (let i = 1; i <= 10; i ++) {
+      expect(stdData.empty()).to.be.false;
+      expect(stdData.get()).to.be.equal('' + i);
+      expect(stdData.size()).to.be.equal(10 - i);
+    }
+
+    expect(stdData.empty()).to.be.true;
+  });
+
+ });
---
+++ src/std-data.ts
@@ -0,0 +1,26 @@
+
+
+export class StdData {
+  private i = 0;
+  private data: string[];
+
+  constructor(content: string) {
+    this.data = content.match(/[^\s]+/g);
+  }
+
+  get(): string {
+    return this.data[this.i++];
+  }
+
+  empty(): boolean {
+    return this.i === this.data.length;
+  }
+
+  size(): number {
+    return this.data.length - this.i;
+  }
+
+  reset(): void {
+    this.i = 0;
+  }
+}
