81b133af54deb2a2cb6cd1af2ae6ac5686cc776b
Evgeny Bobkin <evgeny.bobkin@gmail.com>
2017-03-18 12:17:15 +0100

    extend binary search tests

--- src/binary-search.spec.ts
+++ src/binary-search.spec.ts
@@ -5,16 +5,22 @@ describe('binary seach tests', () => {
   let a = [1, 10, 9, 2, 22, 8, 7, 3, 4, 5, 6];
   // sort test array numerically
   a = a.sort((i, j) => i - j);
-  let bs = BinarySearch.indexOf;
+  const bs = BinarySearch.indexOf;
 
-  it('functionaly verification', () => {
+  it('should match', () => {
     a.forEach(i => {
       const n = bs(a, i);
-      expect(n).to.be.not.equal(-1);
+      expect(n).to.exist;
+      expect(n).to.be.within(0, a.length - 1);
       expect(a[n]).to.be.equal(i);
     });
-    const tt = bs(a, 22);
-    expect(tt).to.be.not.equal(-1);
-    expect(a[tt]).to.be.equal(22);
+  });
+
+  it('should not match', () => {
+    [99, -1, 0].forEach(i => {
+      const n = bs(a, i);
+      expect(n).to.exist;
+      expect(n).to.be.equal(-1);
+    });
   });
 });
