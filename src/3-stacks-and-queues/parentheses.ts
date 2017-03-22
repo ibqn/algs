de0641f4adfcc0d323e59ce33ae2b485079f1322
Evgeny Bobkin <evgeny.bobkin@gmail.com>
2017-03-22 15:12:37 +0100

    add parantheses example

---
+++ src/3-stacks-and-queues/parentheses.ts
@@ -0,0 +1,78 @@
+import * as fs from 'fs';
+import * as yargs from 'yargs';
+
+import { StdData } from '../std-data';
+import { Stack } from '../stack';
+
+
+const LEFT_PAREN     = '(';
+const RIGHT_PAREN    = ')';
+
+const LEFT_BRACE     = '{';
+const RIGHT_BRACE    = '}';
+
+const LEFT_BRACKET   = '[';
+const RIGHT_BRACKET  = ']';
+
+const BRA = [LEFT_PAREN, LEFT_BRACE, LEFT_BRACKET];
+const KET = [RIGHT_PAREN, RIGHT_BRACE, RIGHT_BRACKET];
+
+const isBalanced = (s: string): boolean => {
+  const stack = new Stack<string>();
+
+  for (let c of s) {
+    if (BRA.indexOf(c) !== -1) {
+      stack.push(c);
+      continue;
+    }
+    for (let i = 0; i < KET.length; i ++) {
+      if (c === KET[i]) {
+        if (stack.isEmpty()) {
+          return false;
+        }
+        if (stack.pop() !== BRA[i]) {
+          return false;
+        }
+        continue;
+      }
+    }
+  }
+  return stack.isEmpty();
+};
+
+// Main code
+const main = function() {
+  const argv = yargs
+    .usage('Usage: [options]')
+    .example('$0 -f data.txt', 'Loads data from file')
+    .alias('f', 'file')
+    .nargs('f', 1)
+    .describe('f', 'Specify file with data')
+    .help('h')
+    .alias('h', 'help')
+    .epilog(
+      'parantesis - reads data from a file ' +
+      'and checks to see if the parentheses are balanced.'
+    )
+    .argv;
+
+  let content = ['[()]{}{[()()]()}', '[(])'];
+  if (argv.file) {
+    try {
+      const input = fs.readFileSync(argv.file, {encoding: 'utf8'});
+      const stdData = new StdData(input);
+      content = stdData.get_all();
+    } catch (e) {
+      console.error(`Could not open file '${argv.file}'`);
+    }
+}
+
+  for (let d of content) {
+    console.log(`${d} is${isBalanced(d) ? '' : ' not'} balanced`);
+  }
+};
+
+// Main loop
+if (require.main === module) {
+  main();
+}
