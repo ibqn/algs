9fae16d3f05080762f2835c032cc06de0a52b14b
Evgeny Bobkin <evgeny.bobkin@gmail.com>
2017-03-29 09:57:32 +0200

    add shuffle

---
+++ src/std-random.ts
@@ -0,0 +1,30 @@
+import * as math from 'mathjs';
+
+
+export class StdRandom {
+  // @return a random real number uniformly in [0, 1)
+  public static uniform(): number {
+    return math.random();
+  }
+
+  public static uniformInt(one: number, two?: number): number {
+    if (two) {
+      return math.randomInt(one, two);
+    }
+    return math.randomInt(one);
+  }
+
+  public static shuffle<T>(a: T[]): T[] {
+    const n = a.length;
+    for (let i = 0; i < n; i ++) {
+      const r = StdRandom.uniformInt(i, n);     // between i and n-1
+      const temp: T = a[i];
+      a[i] = a[r];
+      a[r] = temp;
+    }
+    return a;
+  }
+
+  private constructor() {
+  }
+}
